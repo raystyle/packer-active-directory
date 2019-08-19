9c7479ab8a855c6e61739a29bf5d6c0b06ba738e
Dan Quackenbush <dan.quackenbush@contrastsecurity.com>
2019-08-19 14:38:50 -0400

    initial commit

---
+++ scripts/ec2-userdata.ps1
@@ -0,0 +1,20 @@
+<powershell>
+Write-Host "Setting up WinRM"
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$winrm = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $winrm)
+powershell.exe -ExecutionPolicy ByPass -File $winrm
+
+Write-Host "Allowing WinRM in Host Firewall"
+New-NetFirewallRule -Protocol TCP -LocalPort 5986 -Direction Inbound -Action Allow -DisplayName WinRM
+
+# Memory Hotfix
+Write-Host "Applying memory hotfix per ansible documentation"
+$url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
+$file = "$env:temp\Install-WMF3Hotfix.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -Verbose
+
+</powershell>
